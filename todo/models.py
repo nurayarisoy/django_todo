445a9f66dd9a58a388064ddc18ec1beb09af98e0
aaron <aaron@nioyatech.com>
2022-01-29 16:25:11 +0300

    feat: add list

--- todo/models.py
+++ todo/models.py
@@ -16,3 +16,6 @@ class Todo(models.Model):
 
     updated_date = models.DateTimeField(auto_now=True)
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
