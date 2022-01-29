9971b8c3a89e75f734e2f4388a7c0781d63d1884
aaron <aaron@nioyatech.com>
2022-01-29 17:15:58 +0300

    feat: add update

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, todo_list, todo_add
+from .views import home, todo_list, todo_add, todo_update
 
 urlpatterns = [
     path("", home, name="home"),
     path("list/", todo_list, name="list"),
     path("add/", todo_add, name="add"),
+    path("update/<int:id>/", todo_update, name="update"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -34,7 +34,20 @@ def todo_add(request):
 
 
 def todo_update(request, id):
-    pass
+
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {"todo": todo, "form": form}
+
+    return render(request, "todo/todo_update.html", context)
 
 
 def todo_delete(request, id):
